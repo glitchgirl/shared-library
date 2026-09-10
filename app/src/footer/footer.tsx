274c50d9a77eab9cc0570b62c29e86c172c27ccd
Morgan Smith <londonspleen@gmail.com>
2026-09-10 15:46:05 -0400

    Update footer.tsx

---
+++ app/src/footer/footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2026 Library of Things</p>
+      <p>Borrow. Use. Return. Repeat.</p>
+    </footer>
+  );
+}
+
+export default Footer;
